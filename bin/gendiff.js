a3f853cbbc5d0aa6ffb5845de6397df49d4baeba
Антон Селифанов <tatoxer@gmail.com>
2023-03-24 10:48:13 +0300

    Initial

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!usr/env/node
+
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('string-util')
+  .description('CLI to some JavaScript string utilities')
+  .version('0.8.0');
+
+program.parse();
