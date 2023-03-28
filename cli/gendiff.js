639d290728aa2a88cd6a1632c50f134f978b9b2b
Антон Селифанов <tatoxer@gmail.com>
2023-03-28 14:04:06 +0300

    Code refactoring 2

--- cli/gendiff.js
+++ cli/gendiff.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import getSortedJSON from '../scripts/getSortedJSON.js';
 import getAbsPathFile from '../scripts/getAbsoluteFilePath.js';
+import { compareObjectsByEntries, compareObjectsByKeys } from '../scripts/compareObjects.js';
 
 const genDiff = (filePath1, filePath2) => {
   const path1 = getAbsPathFile(filePath1);
@@ -9,38 +10,9 @@ const genDiff = (filePath1, filePath2) => {
   const file1 = getSortedJSON(JSON.parse(fs.readFileSync(path1)));
   const file2 = getSortedJSON(JSON.parse(fs.readFileSync(path2)));
 
-  const entries = Object.entries(file1);
-  const obj2Keys = Object.keys(file2);
+  const obj1Difference = compareObjectsByEntries(file1, file2);
+  const obj2Difference = compareObjectsByKeys(file1, file2);
 
-  const obj1Difference = entries.flatMap(([key, value]) => {
-    const array = [];
-
-    // Если есть ключ но не совпадают значения
-    if (Object.hasOwn(file2, key) && value !== file2[key]) {
-      array.push(`- ${key}: ${value}\n`);
-      array.push(`+ ${key}: ${file2[key]}\n`);
-    }
-
-    // Если совпадают ключи и значения
-    if (Object.hasOwn(file2, key) && value === file2[key]) {
-      array.push(`  ${key}: ${value}\n`);
-    }
-
-    // Если нет ключа
-    if (!Object.hasOwn(file2, key)) {
-      array.push(`- ${key}: ${value}\n`);
-    }
-    return array;
-  });
-
-  const obj2Difference = obj2Keys.flatMap((key) => {
-    const array = [];
-
-    if (!Object.hasOwn(file1, key)) {
-      array.push(`+ ${key}: ${file2[key]}\n`);
-    }
-    return array;
-  });
   return [...obj1Difference, ...obj2Difference].join('');
 };
 
---
+++ scripts/compareObjects.js
@@ -0,0 +1,38 @@
+const compareObjectsByEntries = (obj1, obj2) => {
+  const entries = Object.entries(obj1);
+
+  return entries.flatMap(([key, value]) => {
+    const array = [];
+
+    // Если есть ключ но не совпадают значения
+    if (Object.hasOwn(obj2, key) && value !== obj2[key]) {
+      array.push(`- ${key}: ${value}\n`);
+      array.push(`+ ${key}: ${obj2[key]}\n`);
+    }
+
+    // Если совпадают ключи и значения
+    if (Object.hasOwn(obj2, key) && value === obj2[key]) {
+      array.push(`  ${key}: ${value}\n`);
+    }
+
+    // Если нет ключа
+    if (!Object.hasOwn(obj2, key)) {
+      array.push(`- ${key}: ${value}\n`);
+    }
+    return array;
+  });
+};
+const compareObjectsByKeys = (obj1, obj2) => {
+  const obj2Keys = Object.keys(obj2);
+
+  return obj2Keys.flatMap((key) => {
+    const array = [];
+
+    if (!Object.hasOwn(obj1, key)) {
+      array.push(`+ ${key}: ${obj2[key]}\n`);
+    }
+    return array;
+  });
+};
+
+export { compareObjectsByEntries, compareObjectsByKeys };
