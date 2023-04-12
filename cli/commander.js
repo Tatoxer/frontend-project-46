158a1d81b0b6f343ceb6991ae6d7d6da08260d78
Антон Селифанов <tatoxer@gmail.com>
2023-04-12 11:47:23 +0300

    code processing5

--- cli/commander.js
+++ cli/commander.js
@@ -5,7 +5,7 @@ const commander = () => {
   program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
+    .version('0.0.1')
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format', 'stylish')
     .action((filepath1, filepath2, options) => console.log(
