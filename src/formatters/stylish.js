b6084c96264580d891c0de7256d4629daefeff67
Антон Селифанов <tatoxer@gmail.com>
2023-04-28 10:17:48 +0300

    code refactoring 15

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const makeIndents = (depth, spacesCount, shiftToLeft = 0) => ' '.repeat(depth * spacesCount - shiftToLeft);
+const makeIndents = (depth, spacesCount = 4, shiftToLeft = 2) => ' '.repeat(depth * spacesCount - shiftToLeft);
 
 const stringify = (value, currentDepth) => {
   const iter = (currentValue, depth) => {
@@ -8,16 +8,16 @@ const stringify = (value, currentDepth) => {
       return `${currentValue}`;
     }
 
-    const indent = makeIndents(depth, 4);
+    const indent = makeIndents(depth);
 
     const lines = Object
       .entries(currentValue)
-      .map(([key, val]) => `${indent}    ${key}: ${iter(val, depth + 1)}`);
+      .map(([key, val]) => `${indent}      ${key}: ${iter(val, depth + 1)}`);
 
     return [
       '{',
       ...lines,
-      `${indent}}`,
+      `${indent}  }`,
     ].join('\n');
   };
 
@@ -26,27 +26,26 @@ const stringify = (value, currentDepth) => {
 
 const makeStylishFormat = (coll) => {
   const iter = (currentNode, depth) => {
-    const indent = makeIndents(depth, 4);
-    const indentWithShift = makeIndents(depth, 4, 2);
+    const indent = makeIndents(depth);
 
     const lines = currentNode.map((node) => {
       switch (node.type) {
         case 'changed':
           return [
-            `${indentWithShift}- ${node.key}: ${stringify(node.value1, depth)}`,
-            `${indentWithShift}+ ${node.key}: ${stringify(node.value2, depth)}`,
+            `${indent}- ${node.key}: ${stringify(node.value1, depth)}`,
+            `${indent}+ ${node.key}: ${stringify(node.value2, depth)}`,
           ].join('\n');
         case 'added':
-          return `${indentWithShift}+ ${node.key}: ${stringify(node.value, depth)}`;
+          return `${indent}+ ${node.key}: ${stringify(node.value, depth)}`;
 
         case 'removed':
-          return `${indentWithShift}- ${node.key}: ${stringify(node.value, depth)}`;
+          return `${indent}- ${node.key}: ${stringify(node.value, depth)}`;
 
         case 'unchanged':
-          return `${indent}${node.key}: ${stringify(node.value, depth)}`;
+          return `${indent}  ${node.key}: ${stringify(node.value, depth)}`;
 
         case 'nested':
-          return `${indent}${node.key}: ${iter(node.children, depth + 1)}`;
+          return `${indent}  ${node.key}: ${iter(node.children, depth + 1)}`;
 
         default:
           throw new Error(`${node.type} is unexpected`);
