3efa2aa413565b27a585ebd0230cd89a216d9ed6
Антон Селифанов <tatoxer@gmail.com>
2023-05-02 09:14:58 +0300

    code refactoring 18

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -11,47 +11,47 @@ const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', 
 
 test.each([
   {
-    content1: 'fileDeep1.json',
-    content2: 'fileDeep2.json',
+    fileName1: 'fileDeep1.json',
+    fileName2: 'fileDeep2.json',
     formatter: 'stylish',
     expected: 'expectedDeep.txt',
   },
   {
-    content1: 'fileDeep1.yaml',
-    content2: 'fileDeep2.yml',
+    fileName1: 'fileDeep1.yaml',
+    fileName2: 'fileDeep2.yml',
     formatter: 'stylish',
     expected: 'expectedDeep.txt',
   },
   {
-    content1: 'fileDeep1.yaml',
-    content2: 'fileDeep2.yml',
+    fileName1: 'fileDeep1.yaml',
+    fileName2: 'fileDeep2.yml',
     formatter: undefined,
     expected: 'expectedDeep.txt',
   },
   {
-    content1: 'fileDeep1.json',
-    content2: 'fileDeep2.yml',
+    fileName1: 'fileDeep1.json',
+    fileName2: 'fileDeep2.yml',
     formatter: 'stylish',
     expected: 'expectedDeep.txt',
   },
   {
-    content1: 'fileDeep1.json',
-    content2: 'fileDeep2.json',
+    fileName1: 'fileDeep1.json',
+    fileName2: 'fileDeep2.json',
     formatter: 'plain',
     expected: 'expectedPlain.txt',
   },
   {
-    content1: 'fileDeep1.yaml',
-    content2: 'fileDeep2.yml',
+    fileName1: 'fileDeep1.yaml',
+    fileName2: 'fileDeep2.yml',
     formatter: 'json',
     expected: 'expectedJSON.json',
   },
 ])('.genDiff(%s, %s)', ({
-  content1, content2, formatter, expected,
+  fileName1, fileName2, formatter, expected,
 }) => {
   const rawFileData = fs.readFileSync(getFixturePath(expected), 'utf-8');
   const fileType = path.extname(expected).slice(1);
 
-  expect(genDiff(getFixturePath(content1), getFixturePath(content2), formatter))
+  expect(genDiff(getFixturePath(fileName1), getFixturePath(fileName2), formatter))
     .toEqual(parseFile(rawFileData, fileType));
 });
